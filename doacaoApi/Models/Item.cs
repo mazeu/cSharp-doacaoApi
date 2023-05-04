c775b987b510749f0358f260d1aa17bfb42dd581
mazeu <marciomazeu@hotmail.com>
2023-05-03 23:21:06 -0300

    Começando o projeto

---
+++ doacaoApi/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace doacaoApi.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public string DescricaoLonga { get; set; }
+
+		public double Valor { get; set; }
+
+	}
+}
